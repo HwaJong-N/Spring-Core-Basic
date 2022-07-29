9e92ce82c7aba3948200e24b010968b9997f1a13
ghkwhd <ghkwhd1016@gmail.com>
2022-07-29 22:17:33 +0900

    컨테이너에 등록된 빈 조회

--- core/src/main/java/com/ghkwhd/core/MemberApp.java
+++ core/src/main/java/com/ghkwhd/core/MemberApp.java
@@ -3,6 +3,8 @@ package com.ghkwhd.core;
 import com.ghkwhd.core.member.Grade;
 import com.ghkwhd.core.member.Member;
 import com.ghkwhd.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
@@ -10,8 +12,11 @@ public class MemberApp {
         
         // 자바 코드로 회원가입, 회원 조회 테스트
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- core/src/main/java/com/ghkwhd/core/OrderApp.java
+++ core/src/main/java/com/ghkwhd/core/OrderApp.java
@@ -5,15 +5,21 @@ import com.ghkwhd.core.member.Member;
 import com.ghkwhd.core.member.MemberService;
 import com.ghkwhd.core.order.Order;
 import com.ghkwhd.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
