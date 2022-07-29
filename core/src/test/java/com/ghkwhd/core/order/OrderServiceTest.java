e0522e64086395ce58f1dbbdefbe83f6128f7d76
ghkwhd <ghkwhd1016@gmail.com>
2022-07-29 13:28:22 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ core/src/test/java/com/ghkwhd/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package com.ghkwhd.core.order;
+
+import com.ghkwhd.core.member.Grade;
+import com.ghkwhd.core.member.Member;
+import com.ghkwhd.core.member.MemberService;
+import com.ghkwhd.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
