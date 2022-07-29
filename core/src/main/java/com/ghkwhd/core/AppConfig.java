7675185af1868c75867bbcc45721979880416272
ghkwhd <ghkwhd1016@gmail.com>
2022-07-29 17:51:44 +0900

    AppConfig Refactoring

--- core/src/main/java/com/ghkwhd/core/AppConfig.java
+++ core/src/main/java/com/ghkwhd/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.ghkwhd.core;
 
+import com.ghkwhd.core.discount.DiscountPolicy;
 import com.ghkwhd.core.discount.FixDiscountPolicy;
+import com.ghkwhd.core.member.MemberRepository;
 import com.ghkwhd.core.member.MemberService;
 import com.ghkwhd.core.member.MemberServiceImpl;
 import com.ghkwhd.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import com.ghkwhd.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
